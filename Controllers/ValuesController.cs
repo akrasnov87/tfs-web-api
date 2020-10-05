7a0ab41fc11008b15ca7fefec7d1ccd1cecf3cad
Aleksandr Krasnov <akrasnov87@gmail.com>
2020-10-05 15:41:40 +0300

    update

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace TfsWebAPi.Controllers
 {
@@ -11,5 +12,18 @@ namespace TfsWebAPi.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
     }
 }
